8a647771db04577527b177de7b43ab7f62f165e8
goderyu <goderyu@gmail.com>
2020-02-16 20:25:47 +0800

    Click Play to open door

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find Owning object
+	auto Owner = GetOwner();
+
+	//Find origin rotator
+	auto OriginRotator = Owner->GetActorRotation();
+	//Create a rotator
+	auto NewRotator = FRotator(0.f, OpenAngle, 0.f);
+
+	//Set the door rotation
+
+	Owner->SetActorRotation(OriginRotator + NewRotator);
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PositionReport.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty for Chair!"));
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto OwnerName = GetOwner()->GetName();
+	auto OwnerPosition = 
+		GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, 
+		TEXT("Owner is %s and position is %s"), *OwnerName, *OwnerPosition);
+	// ...
+	
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
