8a574ae8f44c38a410069421d59b100b742c551f
goderyu <goderyu@gmail.com>
2020-02-22 17:59:27 +0800

    Get PhysicsHandle component through the grabber which has the same owner.

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -22,6 +22,18 @@ void UGrabber::BeginPlay()
 	UE_LOG(LogTemp, Warning,
 		TEXT("Grabber reporting for duty"));
 
+	/// Look for attached Physics Handle
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(PhysicsHandle)
+	{
+		/// Physics handle is found
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"), *(GetOwner()->GetName()));
+	}
+	
+
 	// ...
 }
 
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabber.generated.h"
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
@@ -15,14 +16,18 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
-	// How far ahead of the player can we reach
-	UPROPERTY(EditAnywhere)
-		float Reach = 100.f;
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:
 	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+private:
+	// How far ahead of the player can we reach
+	UPROPERTY(EditAnywhere)
+	float Reach = 100.f;
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
 };
